bf724551743bdf3c581fb9f3c5a94d3ec39a3e0d
mc-usercentric <68213914+mc-usercentric@users.noreply.github.com>
2021-03-31 16:58:36 +1100

    amplify

---
+++ queries.js
@@ -0,0 +1,32 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const getNote = /* GraphQL */ `
+  query GetNote($id: ID!) {
+    getNote(id: $id) {
+      id
+      note
+      createdAt
+      updatedAt
+      owner
+    }
+  }
+`;
+export const listNotes = /* GraphQL */ `
+  query ListNotes(
+    $filter: ModelNoteFilterInput
+    $limit: Int
+    $nextToken: String
+  ) {
+    listNotes(filter: $filter, limit: $limit, nextToken: $nextToken) {
+      items {
+        id
+        note
+        createdAt
+        updatedAt
+        owner
+      }
+      nextToken
+    }
+  }
+`;
--- src/App.js
+++ src/App.js
@@ -1,25 +1,148 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import { withAuthenticator, AmplifySignOut } from "@aws-amplify/ui-react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Amplify, {API,graphqlOperation} from 'aws-amplify';
+import aws_exports from './aws-exports'; // specify the location of aws-exports.js file on your project
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+Amplify.configure(aws_exports);
+
+
+const createNote = `mutation createNote($note: String!){
+  createNote(input:{
+    note: $note
+  }){
+    __typename
+    id
+    note
+  }
+}`;
+
+const readNote = `query listNotes{
+  listNotes{
+    items{
+      __typename
+      id
+      note
+    }
+  }
+}`;
+
+const updateNote = `mutation updateNote($id: ID!,$note: String){
+  updateNote(input:{
+    id: $id
+    note: $note
+  }){
+    __typename
+    id
+    note
+  }
+}`;
+
+const deleteNote = `mutation deleteNote($id: ID!){
+  deleteNote(input:{
+    id: $id
+  }){
+    __typename
+    id
+    note
+  }
+}`;
+
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+      id:"",
+      notes:[],
+      value:"",
+      displayAdd:true,
+      displayUpdate:false
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleUpdate = this.handleUpdate.bind(this);
+  }
+
+  async componentDidMount(){
+    const notes = await API.graphql(graphqlOperation(readNote));
+    this.setState({notes:notes.data.listNotes.items});
+  }
+
+  handleChange(event) {
+    this.setState({value:event.target.value});
+  }
+  async handleSubmit(event) {
+    event.preventDefault();
+    event.stopPropagation();
+    const note = {"note":this.state.value}
+    await API.graphql(graphqlOperation(createNote, note));
+    this.listNotes();
+    this.setState({value:""});
+  }
+  async handleDelete(id) {
+    const noteId = {"id":id};
+    await API.graphql(graphqlOperation(deleteNote, noteId));
+    this.listNotes();
+  }
+  async handleUpdate(event) {
+    event.preventDefault();
+    event.stopPropagation();
+    const note = {"id":this.state.id,"note":this.state.value};
+    await API.graphql(graphqlOperation(updateNote, note));
+    this.listNotes();
+    this.setState({displayAdd:true,displayUpdate:false,value:""});
+  }
+  selectNote(note){
+    this.setState({id:note.id,value:note.note,displayAdd:false,displayUpdate:true});
+  }
+  async listNotes(){
+    const notes = await API.graphql(graphqlOperation(readNote));
+    this.setState({notes:notes.data.listNotes.items});
+  }
+  
+  render() {
+    const data = [].concat(this.state.notes)
+      .map((item,i)=> 
+      <div className="alert alert-primary alert-dismissible show" role="alert">
+        <span key={item.i} onClick={this.selectNote.bind(this, item)}>{item.note}</span>
+        <button key={item.i} type="button" className="close" data-dismiss="alert" aria-label="Close" onClick={this.handleDelete.bind(this, item.id)}>
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      )
+    return (
+      <div className="App">
+         <br/>
+        <div className="container">
+          {data}
+        </div>
+      
+        <br/>
+
+        <div className="container">
+          {this.state.displayAdd ?
+            <form onSubmit={this.handleSubmit}>
+              <div className="">
+                <input type="text" className="task" placeholder="Enter task" aria-label="Note" aria-describedby="basic-addon2" value={this.state.value} onChange={this.handleChange}/>
+              </div>
+            </form>
+          : null }
+          {this.state.displayUpdate ?
+            <form onSubmit={this.handleUpdate}>
+              <div className=" mb-3">
+                <input type="text" className="" placeholder="Update Note" aria-label="Note" aria-describedby="basic-addon2" value={this.state.value} onChange={this.handleChange}/>
+                <div className="input-group-append">
+                  <button className="btn btn-primary" type="submit">Update Note</button>
+                </div>
+              </div>
+            </form>
+          : null }
+        </div>
+        <AmplifySignOut />
+      </div>
+    );
+  }
 }
+export default withAuthenticator(App, { includeGreetings: true });
 
-export default App;
