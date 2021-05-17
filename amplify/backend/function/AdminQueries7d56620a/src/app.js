3c29c39f45f6bfecbe789ad1f32fd27b5eb67a7c
Ahmed Hassan <ahmed__hassan@outlook.com>
2021-05-17 17:23:42 +0200

    Update app.js

--- amplify/backend/function/AdminQueries7d56620a/src/app.js
+++ amplify/backend/function/AdminQueries7d56620a/src/app.js
@@ -47,7 +47,7 @@ app.use((req, res, next) => {
 const allowedGroup = process.env.GROUP;
 
 const checkGroup = function(req, res, next) {
-  if (req.path == '/signUserOut') {
+  if (req.path == '/signUserOut' || req.path == '/listUsers' || req.path == '/getUser') {
     return next();
   }
 
