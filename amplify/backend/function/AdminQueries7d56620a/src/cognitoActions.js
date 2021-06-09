c3af33a90f06da80d5be5a917268eba6ad55f8f2
GeeekyBoy <ahmed__hassan@outlook.com>
2021-06-09 17:20:51 +0200

    Bug Fixes

--- amplify/backend/function/AdminQueries7d56620a/src/cognitoActions.js
+++ amplify/backend/function/AdminQueries7d56620a/src/cognitoActions.js
@@ -157,7 +157,6 @@ async function listUsers(Limit, PaginationToken) {
 async function searchForUser(Filter, Limit, PaginationToken) {
   const params = {
     UserPoolId: userPoolId,
-    AttributesToGet: ["sub", "email", "given_name", "family_name"],
     ...(Filter && { Filter: `email ^= "${Filter}"`  }),
     ...(Limit && { Limit }),
     ...(PaginationToken && { PaginationToken }),
