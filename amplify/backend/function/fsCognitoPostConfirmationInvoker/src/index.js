4db0213c01fb57b21644321fed38d3cc23e7fda2
GeeekyBoy <ahmed__hassan@outlook.com>
2021-07-28 01:02:24 +0200

    Bug Fixes

--- amplify/backend/function/fsCognitoPostConfirmationInvoker/src/index.js
+++ amplify/backend/function/fsCognitoPostConfirmationInvoker/src/index.js
@@ -1,24 +1,21 @@
 var aws = require("aws-sdk");
 
+aws.config.region = process.env.REGION
+
 const lambda = new aws.Lambda({
   region: process.env.REGION,
 });
 
-exports.handler = async (event, context) => {
+exports.handler = async (event) => {
   console.log(event)
-  lambda.invoke(
-    {
-      FunctionName: "fsCognitoPostConfirmation",
-      Payload: JSON.stringify(event, null, 2),
-    },
-    function (error, data) {
-      console.log(data)
-      if (error) {
-        context.fail("error", error);
-      }
-      if (data.Payload) {
-        context.succeed(data.Payload);
-      }
-    }
-  );
+  try {
+    const data = await lambda.invoke({
+        FunctionName: "fsCognitoPostConfirmation",
+        InvocationType: "Event",
+        Payload: JSON.stringify(event, null, 2)
+      }).promise();
+    return data;
+  } catch (err) {
+    throw new Error(err)
+  }
 };
