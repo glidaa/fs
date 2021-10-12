5af2cc9b5fcd4f4362eeb036985423283761b2ab
Ahmed Hassan <ahmed__hassan@outlook.com>
2021-10-13 01:23:57 +0200

    Bug Fixes

--- .cracorc.js
+++ .cracorc.js
@@ -1,12 +1,7 @@
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
-
 module.exports = {
   reactScriptsVersion: "react-scripts",
   webpack: {
     configure: {
-      plugins: [
-        new BundleAnalyzerPlugin()
-      ],
       resolve: {
         fallback: {
           stream: require.resolve("stream-browserify"),
