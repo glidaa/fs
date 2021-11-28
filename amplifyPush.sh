cbe5feb6f4129b9265dbdd985a470c9a17c46fac
Ahmed Hassan <ahmed__hassan@outlook.com>
2021-11-28 02:02:39 +0200

    Doing tests

--- amplifyPush.sh
+++ amplifyPush.sh
@@ -32,6 +32,8 @@ init_env () {
         [[ -z ${CATEGORIES} ]] && amplify init --amplify ${AMPLIFY} --providers ${PROVIDERS} --codegen ${CODEGEN} --yes || amplify init --amplify ${AMPLIFY} --providers ${PROVIDERS} --codegen ${CODEGEN} --categories ${CATEGORIES} --yes
         echo "# Environment ${ENV} details:"
         amplify env get --name ${ENV}
+        echo "# Pushing changes to the cloud"
+        amplify push --yes
     fi
     echo "# Done initializing Amplify environment: ${ENV}"
 }
